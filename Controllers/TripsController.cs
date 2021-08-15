f049dab912b412d39694272e4cd6de142efdcbc3
Nicholas Radtke <Raddnc@gmail.com>
2021-08-15 17:50:48 -0500

    created update api endpoint

--- Controllers/TripsController.cs
+++ Controllers/TripsController.cs
@@ -26,5 +26,11 @@ namespace Trips.Controllers
             }
             return Ok();
         }
+        [HttpPut("UpdateTrip/{id}")]
+        public IActionResult UpdateTrip(int id, [FromBody] Trip trip)
+        {
+            _service.UpdateTrip(id, trip);
+            return Ok(trip);
+        }
     }
 }
