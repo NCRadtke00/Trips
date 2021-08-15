42ee8588b65284139de7d6a773b56fdd004ac8ac
Nicholas Radtke <Raddnc@gmail.com>
2021-08-15 17:39:30 -0500

    Add trip api configured

--- Controllers/TripsController.cs
+++ Controllers/TripsController.cs
@@ -3,12 +3,22 @@ using Trips.Data;
 
 namespace Trips.Controllers
 {
+    [Route("api/[controller]")]
     public class TripsController : Controller
     {
         private ITripService _service;
         public TripsController(ITripService service)
         {
             this._service = service;
+        }
+        [HttpPost("AddTrip")]
+        public IActionResult AddTrip([FromBody] Trip trip)
+        {
+            if (trip != null)
+            {
+                _service.AddTrip(trip);
+            }
+            return Ok();
         }
     }
 }
