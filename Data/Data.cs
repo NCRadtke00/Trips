671eb579932443649b921235ddc8479fdc165066
Nicholas Radtke <Raddnc@gmail.com>
2021-08-15 17:25:26 -0500

    creating services

---
+++ Data/Data.cs
@@ -0,0 +1,9 @@
+namespace Trips.Data
+{
+    public static class Data{
+        public static List<Trip> Trips => allTrips;
+        static List<Trip> allTrips = new List<Trip>(){
+            
+        }
+    }
+}
